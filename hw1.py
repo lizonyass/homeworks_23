16726b1cfc0663c2a5208f249e2010333e13e0a5
elizaveta <liza_sheina@rambler.ru>
2023-12-14 21:32:29 +0300

    added hw1

--- hw1.py
+++ hw1.py
@@ -1,9 +1,9 @@
 """Top 3 salaries module."""
 
 
-def top3_salaries_stats(
-    *departments: tuple[str, dict],
-    excluding: tuple[str, ...] = None,
+def top3_salaries(
+    *args,
+    including: tuple[str, ...] = None,
 ) -> tuple[tuple[str, float], tuple[str, float]]:
     """Find 3 most- and least-paid departments in a given tuple by average value.
 
@@ -15,32 +15,14 @@ def top3_salaries_stats(
         stats: tuple of top 3 most and least paid departments with their average salaries.
     """
     avg_salaries = {}
-    for department in departments:
-        if excluding is None or department[0] in excluding:
-            salaries = department[1].values()
+    for dept in args:
+        department, workers = dept
+        if including is None or department in including:
+            salaries = workers.values()
             avg_salary = round(sum(salaries) / len(salaries), 2) if salaries else 0
-            avg_salaries[department[0]] = avg_salary
+            avg_salaries[department] = avg_salary
 
     salaries_sorted = sorted(avg_salaries.items(), key=lambda srt: srt[1], reverse=True)
     top3_most_paid = salaries_sorted[:3]
     top3_least_paid = salaries_sorted[-3:][::-1]
     return tuple(top3_most_paid), tuple(top3_least_paid)
-
-# ----------------------
-# ПРОВЕРКА
-
-dep1 = (
-        'test1', {
-            'test1.1': 231.231,
-            'test1.2': 312.312,
-            'test1.3': 123.123,
-        }
-)
-dep2 = (
-        'test2', {
-            'test2.1': 228.228,
-            'test2.2': 1337.337,
-        }
-)
-
-print(top3_salaries_stats(dep1, dep2))
